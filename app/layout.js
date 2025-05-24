98f39c6acd64d9298238aa808657f824243c84b7
Bryan Herdianto <bryan.herdianto17@gmail.com>
2025-05-24 21:08:57 +0700

    changed font placing

--- app/layout.js
+++ app/layout.js
@@ -22,10 +22,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-          {children}
+        {children}
       </body>
     </html>
   );
